2231d01d15037d6fa3572dc0eb4517bad7f61a43
Brett Crowell <brettcrowell@gmail.com>
2018-02-21 23:38:25 -0500

    add react-color picker and configure

--- src/Swatch.js
+++ src/Swatch.js
@@ -1,17 +1,48 @@
-import React from 'react';
-import {divoomToHex} from "./constants";
+import React, {Component} from 'react';
+import {GithubPicker} from 'react-color';
+import {divoomToHex, hexToDivoom} from "./constants";
 
-export default ({row, col, divoomColor, onChange}) => {
-  return (
-    <div
-      key={divoomColor}
-      style={({
-      backgroundColor: divoomToHex[divoomColor],
-      width: 50,
-      height: 50,
-      marginBottom: 5
-    })}
-      onClick={() => onChange(row, col, divoomColor + 1)}
-    />
-  )
+class Swatch extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {showColorPicker: false};
+    this.onColorPicked = this.onColorPicked.bind(this);
+  }
+
+  onColorPicked({hex}) {
+    const {row, col, onChange} = this.props;
+    onChange(row, col, hexToDivoom[hex]);
+    this.setState({showColorPicker: false})
+  }
+
+  render() {
+    const {divoomColor} = this.props;
+    const {showColorPicker} = this.state;
+    return (
+      <div>
+        <div
+          key={divoomColor}
+          style={({
+            backgroundColor: divoomToHex[divoomColor],
+            width: 50,
+            height: 50,
+            marginBottom: 5
+          })}
+          onClick={() => this.setState({showColorPicker: !showColorPicker})}
+        />
+        {showColorPicker && (
+          <div style={({position: "absolute"})}>
+            <GithubPicker
+              colors={Object.values(divoomToHex)}
+              onChange={this.onColorPicked}
+            />
+          </div>
+        )}
+      </div>
+
+    )
+  }
 }
+
+export default Swatch;
--- src/constants.js
+++ src/constants.js
@@ -7,4 +7,15 @@ export const divoomToHex = {
   5: "#f39",
   6: "#0ff",
   7: "#fff"
-}
+};
+
+export const hexToDivoom = {
+  "#000000": 0,
+  "#ff0000": 1,
+  "#00ff00": 2,
+  "#ffff00": 3,
+  "#0000ff": 4,
+  "#ff3399": 5,
+  "#00ffff": 6,
+  "#ffffff": 7
+};
