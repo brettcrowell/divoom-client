c7a935a774803b12b381cb0472e2830d15450dc9
Brett Crowell <brettcrowell@gmail.com>
2018-02-25 18:30:43 -0500

    basic image handling

--- src/Generate.js
+++ src/Generate.js
@@ -1,8 +1,51 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class Generate extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {file: '', imagePreviewUrl: ''};
+    this.handleImageChange = this.handleImageChange.bind(this)
+  }
+
+  handleImageChange(e) {
+
+    e.preventDefault();
+
+    let reader = new FileReader();
+    let file = e.target.files[0];
+
+    reader.onloadend = () => {
+      this.setState({
+        file: file,
+        imagePreviewUrl: reader.result
+      });
+    };
+
+    reader.readAsDataURL(file)
+  }
+
   render() {
-    return "Generate"
+    let {imagePreviewUrl} = this.state;
+    let $imagePreview = null;
+    if (imagePreviewUrl) {
+      $imagePreview = (<img src={imagePreviewUrl}/>);
+    } else {
+      $imagePreview = (
+        <div className="previewText">Please select an Image for Preview</div>);
+    }
+
+    return (
+      <div className="previewComponent">
+        <input
+          className="fileInput"
+          type="file"
+          onChange={this.handleImageChange}
+        />
+        <div className="imgPreview">
+          {$imagePreview}
+        </div>
+      </div>
+    )
   }
 }
 
