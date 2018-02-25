adc5a6e7b03a7a5244b94bde1bf2abcbba7b8e54
Brett Crowell <brettcrowell@gmail.com>
2018-02-25 18:02:26 -0500

    basic router and generate route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, {Component} from 'react';
+import {Switch, Route} from 'react-router';
+import { BrowserRouter } from 'react-router-dom'
 import Client from './Client';
+import Generate from './Generate';
 import './App.css';
 
 class App extends Component {
 
   render() {
-    return <Client/>
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/generate" component={Generate}/>
+          <Route path="/" component={Client}/>
+        </Switch>
+      </BrowserRouter>
+    )
   }
 
 }
---
+++ src/Generate.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Generate extends Component {
+  render() {
+    return "Generate"
+  }
+}
+
+export default Generate;
