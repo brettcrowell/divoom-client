f52026cf2e870a33b9b8d9ea230fb7c350eac749
Brett Crowell <brettcrowell@gmail.com>
2018-02-25 23:24:55 -0500

    remove routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import React, {Component} from 'react';
-import {Switch, Route} from 'react-router';
-import { BrowserRouter } from 'react-router-dom'
 import Client from './Client';
-import Generate from './Generate';
 import './App.css';
 
 class App extends Component {
 
   render() {
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/generate" component={Generate}/>
-          <Route path="/" component={Client}/>
-        </Switch>
-      </BrowserRouter>
+      <Client/>
     )
   }
 
