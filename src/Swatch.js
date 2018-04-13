3881742ae73fa67bec61c48c69e3d00c27937ac2
Tristan Harward <trisweb@gmail.com>
2018-04-12 23:27:07 -0400

    Add current color picker, painting, dragability, etc.
    Could use cleanup of unused imports etc but /shrug.

--- src/Swatch.js
+++ src/Swatch.js
@@ -6,39 +6,23 @@ class Swatch extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {showColorPicker: false};
-    this.onColorPicked = this.onColorPicked.bind(this);
-  }
-
-  onColorPicked({hex}) {
-    const {row, col, onChange} = this.props;
-    onChange(row, col, hexToDivoom[hex]);
-    this.setState({showColorPicker: false})
   }
 
   render() {
-    const {divoomColor} = this.props;
-    const {showColorPicker} = this.state;
+    const {divoomColor, row, col} = this.props;
     return (
       <div>
         <div
           key={divoomColor}
+          data-row={row}
+          data-col={col}
           style={({
             backgroundColor: divoomToHex[divoomColor],
             width: 50,
             height: 50,
-            marginBottom: 5
+            marginBottom: 0
           })}
-          onClick={() => this.setState({showColorPicker: !showColorPicker})}
         />
-        {showColorPicker && (
-          <div style={({position: "absolute"})}>
-            <GithubPicker
-              colors={Object.values(divoomToHex)}
-              onChange={this.onColorPicked}
-            />
-          </div>
-        )}
       </div>
 
     )
